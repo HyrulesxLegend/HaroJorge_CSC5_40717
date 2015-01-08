fbfa4ce97bfe7df12f551461a1465682a8d15fc1
HyrulesxLegend <jharo13@student.rcc.edu>
2015-01-08 08:05:20 -0800

    more hw

--- Hmwk/Assignment1/Savitch_9thEd_Chap1_ProgProj2/nbproject/Package-Debug.bash
+++ Hmwk/Assignment1/Savitch_9thEd_Chap1_ProgProj2/nbproject/Package-Debug.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/haro_ch1_savitch9th_pro_proj_2
-OUTPUT_BASENAME=haro_ch1_savitch9th_pro_proj_2
-PACKAGE_TOP_DIR=haroch1savitch9thproproj2/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_9thed_chap1_progproj2
+OUTPUT_BASENAME=savitch_9thed_chap1_progproj2
+PACKAGE_TOP_DIR=savitch9thedchap1progproj2/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/haroch1savitch9thproproj2/bin"
+makeDirectory "${NBTMPDIR}/savitch9thedchap1progproj2/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/haroch1savitch9thproproj2.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/savitch9thedchap1progproj2.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/haroch1savitch9thproproj2.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/savitch9thedchap1progproj2.tar *
 checkReturnCode
 
 # Cleanup
