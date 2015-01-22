f45b4fce7b52975a88c5ae2c441f2a351509c3c8
HyrulesxLegend <jharo13@student.rcc.edu>
2015-01-22 11:25:22 -0800

    fixed the program

--- Hmwk/Assignemtn4/Savitch9thEd_Ch4_Prob7/nbproject/Makefile-Debug.mk
+++ Hmwk/Assignemtn4/Savitch9thEd_Ch4_Prob7/nbproject/Makefile-Debug.mk
@@ -64,8 +64,8 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch9thed_ch4_prob7.exe: ${OBJECTF
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
-	${RM} $@.d
-	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
